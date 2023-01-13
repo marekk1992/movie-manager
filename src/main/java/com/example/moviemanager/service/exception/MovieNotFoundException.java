de31cb69b7356598ab2ddc4e596dc77bfe579af7
MarekKomaisko <78044249+marekk1992@users.noreply.github.com>
2023-01-13 12:50:11 +0200

    Revert "Feature_new-rest-api (#1)"
    This reverts commit c8c9c4333b2b94d78904135dd10aa7f79e7aa8bc.

--- src/main/java/com/example/moviemanager/service/exception/MovieNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.moviemanager.service.exception;
-
-public class MovieNotFoundException extends RuntimeException {
-
-    public MovieNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/resources/db/migration/V1__create_movie_table.sql
+++
@@ -1,7 +0,0 @@
-create table movie (
-    id SERIAL primary key,
-    title varchar(100),
-    description varchar(500),
-    release_year int,
-    rating float(2)
-);
